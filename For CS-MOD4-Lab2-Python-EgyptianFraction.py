ec51d9eb5e911e944cddf81f5b91a315b2f0fc95
Anas Jawed <103234658+anasjawed283@users.noreply.github.com>
2023-11-24 18:23:45 +0530

    Add files via upload

---
+++ For CS-MOD4-Lab2-Python-EgyptianFraction.py
@@ -0,0 +1,23 @@
+def egyptian_fraction(numerator, denominator):
+    result = []
+    
+    while numerator > 0:
+        unit_fraction_denominator = -(-denominator // numerator)  # Equivalent to ceil(denominator / numerator)
+        result.append(unit_fraction_denominator)
+        numerator = numerator * unit_fraction_denominator - denominator
+        denominator = denominator * unit_fraction_denominator
+
+    return result
+
+# User input
+numerator = int(input())
+denominator = int(input())
+
+# Call the function to get the result
+result = egyptian_fraction(numerator, denominator)
+
+# Output
+# print(" ".join(map(str, result)))
+print("\n".join(map(str, result)))
+
+
